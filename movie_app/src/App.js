44c658478c799c758e97e8c4a5d6f222f49dc39d
njikim <eunjikim214@gmail.com>
2019-04-23 15:09:18 +0900

    call movie api using ajax and promises

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,76 +2,15 @@ import React, {Component} from 'react';
 import './App.css';
 import Movie from './Movie';
 
-/*
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star wars"
-]
-
-const movieImages = [
-  "https://cdn-images-1.medium.com/max/1600/1*XZrU5omVCZPZXuYv2AZnog.jpeg",
-  "https://images2.minutemediacdn.com/image/upload/c_crop,h_2132,w_3794,x_0,y_22/f_auto,w_1100/v1555155644/shape/mentalfloss/full-metal-jacket-hed.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-  "https://starwarsblog.starwars.com/wp-content/uploads/2019/03/star-wars-celebration-chicago-lightspeed-tall-C.jpg"
-]
-
-
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://cdn-images-1.medium.com/max/1600/1*XZrU5omVCZPZXuYv2AZnog.jpeg"
-  },
-  {
-    title: "full Metal Jacket",
-    poster: "https://images2.minutemediacdn.com/image/upload/c_crop,h_2132,w_3794,x_0,y_22/f_auto,w_1100/v1555155644/shape/mentalfloss/full-metal-jacket-hed.jpg",
-
-  },
-  {
-    title: "Old boy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-
-  },
-  {
-    title: "Star wars",
-    poster: "https://starwarsblog.starwars.com/wp-content/uploads/2019/03/star-wars-celebration-chicago-lightspeed-tall-C.jpg"
-
-  }
-]
-*/
-
 class App extends Component {
   state = {
   }
   
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies : [
-          {
-            title: "Matrix",
-            poster: "https://cdn-images-1.medium.com/max/1600/1*XZrU5omVCZPZXuYv2AZnog.jpeg"
-          },
-          {
-            title: "full Metal Jacket",
-            poster: "https://images2.minutemediacdn.com/image/upload/c_crop,h_2132,w_3794,x_0,y_22/f_auto,w_1100/v1555155644/shape/mentalfloss/full-metal-jacket-hed.jpg",
-        
-          },
-          {
-            title: "Old boy",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-        
-          },
-          {
-            title: "Star wars",
-            poster: "https://starwarsblog.starwars.com/wp-content/uploads/2019/03/star-wars-celebration-chicago-lightspeed-tall-C.jpg"
-        
-          }
-        ]
-      })
-      console.log('hi')
-    }, 5000)
+    fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log('error'))
   }
 
   _renderMovies = () => {
