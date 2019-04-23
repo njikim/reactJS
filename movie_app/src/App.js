2382a369f1b3ccb5dddd8464bec5bbec4e4a45e7
njikim <eunjikim214@gmail.com>
2019-04-23 13:00:03 +0900

    add movie using this.setState() after 5 sec

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -16,7 +16,7 @@ const movieImages = [
   "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
   "https://starwarsblog.starwars.com/wp-content/uploads/2019/03/star-wars-celebration-chicago-lightspeed-tall-C.jpg"
 ]
-*/
+
 
 const movies = [
   {
@@ -39,12 +39,53 @@ const movies = [
 
   }
 ]
+*/
 
 class App extends Component {
+  state = {
+    movies : [
+      {
+        title: "Matrix",
+        poster: "https://cdn-images-1.medium.com/max/1600/1*XZrU5omVCZPZXuYv2AZnog.jpeg"
+      },
+      {
+        title: "full Metal Jacket",
+        poster: "https://images2.minutemediacdn.com/image/upload/c_crop,h_2132,w_3794,x_0,y_22/f_auto,w_1100/v1555155644/shape/mentalfloss/full-metal-jacket-hed.jpg",
+    
+      },
+      {
+        title: "Old boy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+    
+      },
+      {
+        title: "Star wars",
+        poster: "https://starwarsblog.starwars.com/wp-content/uploads/2019/03/star-wars-celebration-chicago-lightspeed-tall-C.jpg"
+    
+      }
+    ]
+  }
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          //...this.state.movies, //이전 영화를 그대로 두고, 한개 영화를 추가.
+          {
+            title: "Transporting",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Trainspotting_ver2.jpg/220px-Trainspotting_ver2.jpg"
+          },
+          ...this.state.movies,
+        ]
+      })
+      console.log('hi')
+    }, 5000)
+  }
+
+
   render(){
     return (
     <div className="App">
-      {movies.map((movie, index) => {
+      {this.state.movies.map((movie, index) => {
         return <Movie title={movie.title} poster={movie.poster} key={index} />
       })}
       
