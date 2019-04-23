91741befe6a325c7e545fa32d9e8b2c20c29b930
njikim <eunjikim214@gmail.com>
2019-04-23 15:35:00 +0900

    await & async

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,22 +3,34 @@ import './App.css';
 import Movie from './Movie';
 
 class App extends Component {
-  state = {
-  }
+  state = {}
   
   componentDidMount(){
-    fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log('error'))
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies
   }
+
+  // asyncronized function
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=rating")
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log('error'))
+  }
+
   render(){
     return (
     <div className="App">
